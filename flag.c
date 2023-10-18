05fdaff41b379e90558fe6f60a9f37e6687685c4
NosiphoZulu <140641051+NosiphoZulu@users.noreply.github.com>
2023-10-18 17:34:28 +0200

    Update flag.c

--- flag.c
+++ flag.c
@@ -2,7 +2,7 @@
 
 /**
  * get_flags - Calculates active flags
- * @format: Formatted string in which to print the arguments
+ * @format: Formatted string in which to print the arguments and take the parameter.
  * @i: take a parameter.
  * Return: Flags:
  */
